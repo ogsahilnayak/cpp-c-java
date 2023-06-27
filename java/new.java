46398f2981b8d64f7aa1aad4b07846afa6f865f1
ogsahilnayak <contactsahilnayakgmail.com>
2023-06-27 23:46:02 +0530

    initial code

--- java/new.java
+++ java/new.java
@@ -1,4 +1,3 @@
-package java;
 class Hello{
     public static void main(String[] args) {
         System.out.println("hello world");
--- java/variables.java
+++ java/variables.java
@@ -1,4 +1,4 @@
-package java;
+
 
 public class variables {
     public static void main(String[] args) {
