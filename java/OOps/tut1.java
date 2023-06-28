231762f9c2df8282085d8f367b09aed8be166452
ogsahilnayak <contactsahilnayakgmail.com>
2023-06-28 17:38:22 +0530

    initial code

---
+++ java/OOps/tut1.java
@@ -0,0 +1,11 @@
+public class Hello{
+    public static void main(String[] args) { // public means we can access it from anywhere
+        //static help us to call a method or function with creating a object of class.
+        //void means nothing 
+        //main is the method/function
+        //string[] args are parameters
+        System.out.println("hello world"); //it will give output --> hello world and ln means next line
+        System.out.print("sahil\n");// \n is used for new line
+        System.out.println("nayak");// print will give output in same line
+    }
+}
