0ee2fd7ad33a0c91f6c7f2867c1a36a1c8e1e532
ogsahilnayak <contactsahilnayakgmail.com>
2023-06-28 10:55:56 +0530

    changes

---
+++ cpp/if-else.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num;
+    cout<<"enter a number: ";
+    cin>>num;
+
+    if (num%2==0){
+        cout<<"its even!!";
+    }
+    else{
+        cout<<"its odd!!";
+    }
+
+
+
+    return 0;
+}
