8953d31dde3a639392ff40ce57a94b956a52dd9e
ogsahilnayak <contactsahilnayakgmail.com>
2023-06-28 12:28:31 +0530

    initial code

--- java/variable.java
+++ java/variable.java
@@ -1,6 +1,11 @@
 public class variable {
     public static void main(String[] args) {
-        int a = 97;
+        int a = 97; // integer value
+        boolean flag = true; // boolean value 0---> false 1--->true
+        float rate = 4.6f;
+        
+        System.out.println(rate);
+        System.out.println(flag);
         System.out.println(a);
     }
 }
