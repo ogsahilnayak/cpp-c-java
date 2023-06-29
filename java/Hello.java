a57e950d8de4007deb8922e7f05a5dcc18f9ba0f
ogsahilnayak <contactsahilnayakgmail.com>
2023-06-29 23:39:57 +0530

    new code

--- java/Hello.java
+++ java/Hello.java
@@ -1,4 +1,4 @@
-public class Hello{
+public class Hello{ // class name should be always <filename> 
     public static void main(String[] args) { // public means we can access it from anywhere
         //static help us to call a method or function with creating a object of class.
         //void means nothing 
@@ -6,6 +6,8 @@ public class Hello{
         //string[] args are parameters
         System.out.println("hello world"); //it will give output --> hello world and ln means next line
         System.out.print("sahil\n");// \n is used for new line
+        //print keyword is used to print the output in same line.
         System.out.println("nayak");// print will give output in same line
+        // out means output!
     }
 }
